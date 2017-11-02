982272dcc42cdf0ad2fe14bc69c26f700505572e
ZebanNikolay <zebannikolay@gmail.com>
2017-11-02 14:56:08 +0900

    Remove TagEditorActivity, menu and chinese string

--- demo/src/main/java/me/gujun/android/taggroup/demo/MainActivity.java
+++ demo/src/main/java/me/gujun/android/taggroup/demo/MainActivity.java
@@ -1,10 +1,7 @@
 package me.gujun.android.taggroup.demo;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -40,12 +37,6 @@ public class MainActivity extends AppCompatActivity {
 
         mPromptText = (TextView) findViewById(R.id.tv_prompt);
         mPromptText.setVisibility((tags == null || tags.length == 0) ? View.VISIBLE : View.GONE);
-        mPromptText.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                launchTagEditorActivity();
-            }
-        });
 
         mDefaultTagGroup = (TagGroup) findViewById(R.id.tag_group);
         mSmallTagGroup = (TagGroup) findViewById(R.id.tag_group_small);
@@ -59,14 +50,6 @@ public class MainActivity extends AppCompatActivity {
             mBeautyTagGroup.setTags(tags);
             mBeautyInverseTagGroup.setTags(tags);
         }
-
-        MyTagGroupOnClickListener tgClickListener = new MyTagGroupOnClickListener();
-
-        mDefaultTagGroup.setOnClickListener(tgClickListener);
-        mSmallTagGroup.setOnClickListener(tgClickListener);
-        mLargeTagGroup.setOnClickListener(tgClickListener);
-        mBeautyTagGroup.setOnClickListener(tgClickListener);
-        mBeautyInverseTagGroup.setOnClickListener(tgClickListener);
 
         mDefaultTagGroup.setOnTagClickListener(mTagClickListener);
         mSmallTagGroup.setOnTagClickListener(mTagClickListener);
@@ -87,30 +70,4 @@ public class MainActivity extends AppCompatActivity {
         mBeautyInverseTagGroup.setTags(tags);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main_activity, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == R.id.action_edit) {
-            launchTagEditorActivity();
-            return true;
-        }
-        return false;
-    }
-
-    protected void launchTagEditorActivity() {
-        Intent intent = new Intent(MainActivity.this, TagEditorActivity.class);
-        startActivity(intent);
-    }
-
-    class MyTagGroupOnClickListener implements View.OnClickListener {
-        @Override
-        public void onClick(View v) {
-            launchTagEditorActivity();
-        }
-    }
 }
